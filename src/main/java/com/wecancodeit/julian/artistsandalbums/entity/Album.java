e457ec0d07a4f602cebf741bf79724d73faeff1e
julian-perge <dpstank@gmail.com>
2018-07-23 05:11:14 -0400

    Modified mapping to artist from ManyToOne, to ManyToMany. Added ManyToOne mapping to Band. Added ManyToOne mapping to Record Label. Revmoed hashCode and equals override. Modified constructor to include Band, Record Label, and list of Artists.

--- src/main/java/com/wecancodeit/julian/artistsandalbums/entity/Album.java
+++ src/main/java/com/wecancodeit/julian/artistsandalbums/entity/Album.java
@@ -1,14 +1,16 @@
 package com.wecancodeit.julian.artistsandalbums.entity;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -18,92 +20,51 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "ALBUMS")
 public class Album {
-  @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   private String albumName;
+
+  @ManyToMany private Collection<Artist> artists;
+
+  @ManyToOne private Band band;
+
+  @OneToMany(mappedBy = "album")
+  @Column(name = "ALBUM_COMMENTS")
+  private Collection<Comment> comments;
+
   private String coverImage;
   private String genre;
+  @ManyToOne private RecordLabel recordLabel;
   private String releaseDate;
 
   @OneToMany(mappedBy = "album")
   @JsonIgnore
   private Collection<Song> songs;
-  
-  @OneToMany(mappedBy="album")
-  @Column(name="ALBUM_COMMENTS")
-  private Collection<Comment> comments;
-
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "ARTIST_ID", nullable = false)
-  @JsonIgnore
-  private Artist artist;
 
   /**
    * @param albumName
-   * @param releaseDate
-   * @param genre
    * @param coverImage
+   * @param genre
+   * @param releaseDate
    * @param songs
    */
   public Album(
-      String albumName, String releaseDate, String genre, String coverImage, Artist artist) {
+      String albumName,
+      Band band,
+      String coverImage,
+      String genre,
+      RecordLabel recordLabel,
+      String releaseDate,
+      Artist... artists) {
     this.albumName = albumName;
-    this.releaseDate = releaseDate;
-    this.genre = genre;
+    this.artists = Arrays.asList(artists);
+    this.band = band;
     this.coverImage = coverImage;
-    this.artist = artist;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((albumName == null) ? 0 : albumName.hashCode());
-    result = prime * result + ((artist == null) ? 0 : artist.hashCode());
-    result = prime * result + ((coverImage == null) ? 0 : coverImage.hashCode());
-    result = prime * result + ((genre == null) ? 0 : genre.hashCode());
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
-    result = prime * result + ((songs == null) ? 0 : songs.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    Album other = (Album) obj;
-    if (albumName == null) {
-      if (other.albumName != null) return false;
-    } else if (!albumName.equals(other.albumName)) return false;
-    if (artist == null) {
-      if (other.artist != null) return false;
-    } else if (!artist.equals(other.artist)) return false;
-    if (coverImage == null) {
-      if (other.coverImage != null) return false;
-    } else if (!coverImage.equals(other.coverImage)) return false;
-    if (genre == null) {
-      if (other.genre != null) return false;
-    } else if (!genre.equals(other.genre)) return false;
-    if (id == null) {
-      if (other.id != null) return false;
-    } else if (!id.equals(other.id)) return false;
-    if (releaseDate == null) {
-      if (other.releaseDate != null) return false;
-    } else if (!releaseDate.equals(other.releaseDate)) return false;
-    if (songs == null) {
-      if (other.songs != null) return false;
-    } else if (!songs.equals(other.songs)) return false;
-    return true;
-  }
-  
-  public Collection<Comment> getComments() {
-	  return comments;
-  }
-
-  public Artist getArtist() {
-    return artist;
+    this.genre = genre;
+    this.recordLabel = recordLabel;
+    this.releaseDate = releaseDate;
   }
 
   public Long getId() {
@@ -114,12 +75,28 @@ public class Album {
     return albumName;
   }
 
+  public Collection<Artist> getArtists() {
+    return artists;
+  }
+
+  public Band getBand() {
+    return band;
+  }
+
+  public Collection<Comment> getComments() {
+    return comments;
+  }
+
   public String getCoverImage() {
     return coverImage;
   }
 
   public String getGenre() {
     return genre;
+  }
+
+  public RecordLabel getRecordLabel() {
+    return recordLabel;
   }
 
   public String getReleaseDate() {
