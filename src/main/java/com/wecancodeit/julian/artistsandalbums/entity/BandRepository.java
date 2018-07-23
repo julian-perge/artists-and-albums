b99ca44aa38ec4aeb41358517c4d3c76444b04ec
julian-perge <dpstank@gmail.com>
2018-07-23 04:16:09 -0400

    Added proper title text for album. Added links to artists and bands. Added section for album cover and put songs in it's own section. Also put album comments in a section. Added footer.

--- src/main/java/com/wecancodeit/julian/artistsandalbums/entity/BandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wecancodeit.julian.artistsandalbums.entity;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BandRepository extends CrudRepository<Band, Long> {}
